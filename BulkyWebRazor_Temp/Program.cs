280012784c97e09630161889a82e058d9277cc69
Tauqueer Alam <tauqueeralam42@gmail.com>
2025-01-05 18:40:01 +0530

    Setup EF Core with PostgreSQL and add Razor Pages
    Configured ApplicationDbContext to use PostgreSQL with the "DefaultConnection" connection string. Added Razor Pages services to the service container. Built the WebApplication instance to handle HTTP requests.

--- BulkyWebRazor_Temp/Program.cs
+++ BulkyWebRazor_Temp/Program.cs
@@ -1,7 +1,14 @@
+using BulkyWebRazor_Temp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
